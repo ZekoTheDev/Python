0f1025d6c386b4029499b015d90b9f518a6994f5
zekothedev <z.erassan@gmail.com>
2022-09-13 18:34:15 +0200

    Add welcomeMessage Program

---
+++ welcomeMessage.py
@@ -0,0 +1,5 @@
+print("Hi, What is your name?")
+name = input("Fill in name: ")
+print("And how old are you ?")
+age = int(input("Fill in your age: "));
+print(f"Hello {name} you will be turning {age + 1} soon!")
