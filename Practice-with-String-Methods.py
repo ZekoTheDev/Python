db5be1ae01ce9fa528bd363fa24e102bc340cca5
zekothedev <z.erassan@gmail.com>
2022-09-26 18:48:54 +0200

    Add Practice with Strings & List Files

---
+++ Practice-with-String-Methods.py
@@ -0,0 +1,18 @@
+events = '9/13 2:30 PM\n9/14 11:15 AM\n9/14 1:00 PM\n9/15 9:00 AM'
+
+# a the number of events on 9/14
+
+print(events.count("9/14"))
+
+# b the index of the substring describing the 1st event on 9/14
+
+print(events.find("9/14")) # The Index() method could be used here aswell
+
+# c the index just past the substring describing the last event on 9/14
+
+print(events.find("9/15")) # The Index() method could be used here aswell
+
+# d the list of substrings describing the events on 9/14 
+
+# Don't really understand this for 'now', coming back to question D later.
+
---
+++ Practicing-with-Lists-Indexes.py
@@ -0,0 +1,12 @@
+# Practicing with Lists Indexes
+
+lst = ['a','b','c','d','e','f','g','h']
+      # 0   1   2   3   4   5   6   7
+      
+print(lst[:4])
+print(lst[3:6])
+print(lst[3:4])
+print(lst[5:7])
+print(lst[3:8])
+print(lst[5:])
+
