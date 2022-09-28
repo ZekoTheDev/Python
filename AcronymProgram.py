615da9c4eba413599a6128de5f8f8a01592f6ff2
zekothedev <z.erassan@gmail.com>
2022-09-28 10:33:57 +0200

    Add Description and Capitalize output

--- AcronymProgram.py
+++ AcronymProgram.py
@@ -1,7 +1,9 @@
+# With this program the first letter of Each word will be saved, capitalized and returned. Example: random access memory becomes, RAM.
+
 def acronym (phrase):
-    userPhrase = phrase.split()
+    userPhrase = phrase.upper().split()
     print(userPhrase)
     for word in userPhrase:
         print(word[0][0] , end=' ')
 
-acronym("Central Processing Unit")
+acronym("central processing unit")
