ffe7d87e33aaff7077460144f361faf7e27fbee1
zekothedev <z.erassan@gmail.com>
2022-10-04 22:00:08 +0200

    Add gradesFilter Program

---
+++ gradesFilter.py
@@ -0,0 +1,12 @@
+# Input a dictionary that contains a key/value a pair of student/grade. This Program will return the students that scored higher than 9.0
+
+myStudentList = {'Bruce Lee': 9.4, 'Muhammed Ali': 9.8, 'John Doe': 8.9}
+
+def gradeFilter(studentList):
+    newDict = dict()
+    for (key, value) in studentList.items():
+        if value > 9.0:
+            newDict[key] = value
+    return newDict
+
+print(gradeFilter(myStudentList))
