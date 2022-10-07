f6c3710d09ad3702d170162e90f2e1def09cc2e0
zekothedev <z.erassan@gmail.com>
2022-10-07 23:54:32 +0200

    Fix spelling

--- gradesFilter.py
+++ gradesFilter.py
@@ -1,4 +1,4 @@
-# Input a dictionary that contains a key/value a pair of student/grade. This Program will return the students that scored higher than 9.0
+# Input a dictionary that contains a key/value a pair of student/grade. This Program will return the students that scored higher than 9.0.
 
 myStudentList = {'Bruce Lee': 9.4, 'Muhammed Ali': 9.8, 'John Doe': 8.9}
 
