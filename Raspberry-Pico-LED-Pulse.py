12d4009a19628063bc9a25830c405f08df2799ed
zekothedev <z.erassan@gmail.com>
2022-11-30 09:37:56 +0100

    Add Pulse Program that works with the Pi Pico

---
+++ Raspberry-Pico-LED-Pulse.py
@@ -0,0 +1,19 @@
+from machine import Pin
+import time
+
+gpio_pin = Pin(25, Pin.OUT)
+
+
+def pulse(pin, high_time, low_time):
+    # Geef een puls op de pin:
+    # Maak de pin pin_nr hoog, wacht high_time,
+    gpio_pin.value(1)
+    time.sleep(high_time)
+    # maak de pin laag, en wacht nog low_time
+    gpio_pin.value(0)
+    time.sleep(low_time)
+    # implementeer deze functie
+
+
+while True:
+    pulse(gpio_pin, 0.2, 0.2)
