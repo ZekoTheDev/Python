67f5b786adc1aed5c68f14ef885525c07c4211a7
zekothedev <z.erassan@gmail.com>
2022-12-01 22:37:13 +0100

    Add Pico Pulse Program, which allows Pi Pico to show pulse trough LED

---
+++ pulse-pico.py
@@ -0,0 +1,15 @@
+from machine import Pin
+import time
+
+gpio_pin = Pin(25, Pin.OUT)
+
+
+def pulse(pin, high_time, low_time):
+    gpio_pin.value(1)
+    time.sleep(high_time)
+    gpio_pin.value(0)
+    time.sleep(low_time)
+
+
+while True:
+    pulse(gpio_pin, 0.2, 0.2)
